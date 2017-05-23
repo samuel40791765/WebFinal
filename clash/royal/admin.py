81b049d849353a5c8ab6cdfa387fd14bf07e7e63
B10332024 <samuel19960920@yahoo.com>
2017-05-23 10:25:45 +0800

    add card model

--- clash/royal/admin.py
+++ clash/royal/admin.py
@@ -5,3 +5,4 @@ from .models import *
 
 
 admin.site.register(UpdateInfo)
+admin.site.register(card)
--- clash/royal/models.py
+++ clash/royal/models.py
@@ -9,3 +9,24 @@ class UpdateInfo(models.Model):
 	ImgName = models.CharField(max_length=20)
 	def __str__(self):
 		return self.title
+
+class card(models.Model):
+	RARITY_CHOICES = (
+    	('C', 'Common'),
+    	('R', 'Rare'),
+    	('E', 'Epic'),
+    	('L', 'Legendary'),
+	)
+	TYPE_CHOICES = (
+    	('T', 'Troop'),
+    	('S', 'Spell'),
+    	('B', 'Building'),
+	)
+	name=models.CharField(max_length=20,blank=False)
+	rarity=models.CharField(max_length=20,choices=RARITY_CHOICES,blank=False)
+	cost=models.PositiveIntegerField(blank=False)
+	arena=models.PositiveIntegerField(blank=False)
+	cardtype=models.CharField(max_length=20,choices=TYPE_CHOICES,blank=False)
+	ImgUrl=models.CharField(max_length=30,blank=True)
+	def __str__(self):
+		return self.name
