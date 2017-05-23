de8e0eeb61575626ad0ae41f534960ffc7f6dcc9
B10332024 <samuel19960920@yahoo.com>
2017-05-23 23:21:43 +0800

    deck model change

--- clash/royal/models.py
+++ clash/royal/models.py
@@ -28,5 +28,6 @@ class card(models.Model):
 	arena=models.PositiveIntegerField(blank=False)
 	cardtype=models.CharField(max_length=20,choices=TYPE_CHOICES,blank=False)
 	ImgUrl=models.CharField(max_length=30,blank=True)
+	indeck=models.BooleanField(default=False)
 	def __str__(self):
 		return self.name
--- clash/royal/views.py
+++ clash/royal/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render,render_to_response
+from .models import *
+from django import template
 
 # Create your views here.
 def index(request):
 	return render_to_response('index.html',locals())
 
 def deck(request):
-	return render_to_response('deck.html',locals())
+	card_list = card.objects.order_by('cost')
+	context={'card_list':card_list}
+	return render(request,'deck.html',context)
 
 def card_rank(request):
 	return render_to_response('card_rank.html', locals())
 
 def generic(request):
 	return render_to_response('generic.html', locals())
+
