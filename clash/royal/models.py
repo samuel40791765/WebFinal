704b0f8308143be4b5503ca11a081595cb5fe7f5
B10332024 <samuel19960920@yahoo.com>
2017-05-28 23:17:21 +0800

    deckalmostdone

--- clash/royal/models.py
+++ clash/royal/models.py
@@ -25,9 +25,16 @@ class CardInfo(models.Model):
 		return self.idName
 
 class Deck(models.Model):
-	cost=models.IntegerField()
+	cost=models.FloatField()
 	name=models.CharField(max_length=20)
 	description = models.TextField(max_length=200)
-	board = ArrayField(models.IntegerField(blank=True),size=8)
+	card1=models.CharField(max_length=20)
+	card2=models.CharField(max_length=20)
+	card3=models.CharField(max_length=20)
+	card4=models.CharField(max_length=20)
+	card5=models.CharField(max_length=20)
+	card6=models.CharField(max_length=20)
+	card7=models.CharField(max_length=20)
+	card8=models.CharField(max_length=20)
 	def __str__(self):
 		return self.name
--- clash/royal/views.py
+++ clash/royal/views.py
@@ -1,5 +1,5 @@
 from __future__ import division
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render,render_to_response,redirect,get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from .forms import *
 from .models import *
@@ -10,26 +10,11 @@ def index(request):
 
 @csrf_exempt
 def deck(request):
-	if request.method == "POST":
-		checkdeck = CardInfo.objects.order_by('elixirCost')
-		currentDeck = []
-		if request.POST['action']=="todeck":
-			for cards in checkdeck:
-				if cards.inDeck:
-					currentDeck.append(cards.id)
-			if len(currentDeck)<8:
-				value=request.POST.get("value")
-				b=CardInfo.objects.get(id=str(value))
-				b.inDeck=True
-				b.save()
-		if request.POST['action']=="outdeck":
-			value=request.POST.get("value")
-			b=CardInfo.objects.get(id=str(value))
-			b.inDeck=False
-			b.save()
+	form = DeckForm(prefix="deck")
 	card_list = CardInfo.objects.order_by('elixirCost')
 	cost=0
 	Deck=[]
+	enough=True;
 	for deckcard in card_list:
 		if deckcard.inDeck:
 			cost+=deckcard.elixirCost
@@ -39,10 +24,213 @@ def deck(request):
 		averagecost=round(averagecost, 2)
 	else:
 		averagecost=0
-	form = DeckForm()
-	context={'card_list':card_list,'averagecost':averagecost,'form':form}
+
+	if request.method == "POST":
+		checkdeck = CardInfo.objects.order_by('elixirCost')
+		currentDeck = []
+		form = DeckForm(request.POST, prefix="deck")
+		if form.is_valid() and len(Deck)== 8 and 'action' not in request.POST:
+			deck=form.save(commit=False)
+			deck.cost=averagecost
+			deck.card1=Deck[0].idName
+			deck.card2=Deck[1].idName
+			deck.card3=Deck[2].idName
+			deck.card4=Deck[3].idName
+			deck.card5=Deck[4].idName
+			deck.card6=Deck[5].idName
+			deck.card7=Deck[6].idName
+			deck.card8=Deck[7].idName
+			for card in Deck:
+				temp=CardInfo.objects.get(id=str(card.id))
+				temp.inDeck=False
+				temp.save()
+			deck.save()
+			return redirect('mydecks')
+		elif form.is_valid() and len(Deck)!= 8:
+			enough=False
+		elif request.POST['action']=="todeck":
+			for cards in checkdeck:
+				if cards.inDeck:
+					currentDeck.append(cards.id)
+			if len(currentDeck)<8:
+				value=request.POST.get("value")
+				b=CardInfo.objects.get(id=str(value))
+				b.inDeck=True
+				b.save()
+		elif request.POST['action']=="outdeck":
+			value=request.POST.get("value")
+			b=CardInfo.objects.get(id=str(value))
+			b.inDeck=False
+			b.save()
+	context={'card_list':card_list,'averagecost':averagecost,'form':form,'enough':enough,}
 	return render(request, 'deck.html', context)
 
+def mydecks(request):
+	deck_list = Deck.objects.order_by('name')
+	card_list = CardInfo.objects.order_by('id')
+	context={'deck_list':deck_list,'card_list':card_list}
+	return render(request, 'mydecks.html', context)
+
+@csrf_exempt
+def deck_edit(request,pk):
+	deck = get_object_or_404(Deck, pk=pk)
+	card_list = CardInfo.objects.order_by('elixirCost')
+	cost=0
+	cards=0
+	enough=True;
+	for deckcard in card_list:
+		if deck.card1==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+		elif deck.card2==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+		elif deck.card3==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+		elif deck.card4==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+		elif deck.card5==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+		elif deck.card6==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+		elif deck.card7==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+		elif deck.card8==deckcard.idName:
+			cost+=deckcard.elixirCost
+			cards+=1
+
+	if request.method == "POST":
+		form = DeckForm(request.POST, instance=deck)
+		if form.is_valid() and cards==8 and 'action' not in request.POST:
+			deck = form.save(commit=False)
+			deck.save()
+			return redirect('mydecks')
+		elif form.is_valid() and cards!=8 and 'action' not in request.POST:
+			enough=False
+		elif request.POST['action']=="todeck":
+			value=request.POST.get("value")
+			if deck.card1=='null':
+				deck.card1=value
+				for deckcard in card_list:
+					if deck.card1==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			elif deck.card2=="null":
+				deck.card2=value
+				for deckcard in card_list:
+					if deck.card2==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			elif deck.card3=="null":
+				deck.card3=value
+				for deckcard in card_list:
+					if deck.card3==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			elif deck.card4=="null":
+				deck.card4=value
+				for deckcard in card_list:
+					if deck.card4==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			elif deck.card5=="null":
+				deck.card5=value
+				for deckcard in card_list:
+					if deck.card5==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			elif deck.card6=="null":
+				deck.card6=value
+				for deckcard in card_list:
+					if deck.card6==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			elif deck.card7=="null":
+				deck.card7=value
+				for deckcard in card_list:
+					if deck.card7==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			elif deck.card8=="null":
+				deck.card8=value
+				for deckcard in card_list:
+					if deck.card8==deckcard.idName:
+						cost+=deckcard.elixirCost
+						cards+=1
+			if cards>0:
+				averagecost=float(cost) /cards
+				averagecost=round(averagecost, 2)
+			else:
+				averagecost=0
+			deck.cost=averagecost
+			deck.save()
+		elif request.POST['action']=="outdeck":
+			value=request.POST.get("value")
+			if deck.card1 == value:
+				deck.card1='null'
+				for deckcard in card_list:
+					if deck.card1==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			elif deck.card2==value:
+				deck.card2="null"
+				for deckcard in card_list:
+					if deck.card2==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			elif deck.card3==value:
+				deck.card3="null"
+				for deckcard in card_list:
+					if deck.card3==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			elif deck.card4==value:
+				deck.card4="null"
+				for deckcard in card_list:
+					if deck.card4==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			elif deck.card5==value:
+				deck.card5="null"
+				for deckcard in card_list:
+					if deck.card5==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			elif deck.card6==value:
+				deck.card6="null"
+				for deckcard in card_list:
+					if deck.card6==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			elif deck.card7==value:
+				deck.card7="null"
+				for deckcard in card_list:
+					if deck.card7==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			elif deck.card8==value:
+				deck.card8="null"
+				for deckcard in card_list:
+					if deck.card8==deckcard.idName:
+						cost-=deckcard.elixirCost
+						cards-=1
+			if cards>0:
+				averagecost=float(cost) /cards
+				averagecost=round(averagecost, 2)
+			else:
+				averagecost=0
+			deck.cost=averagecost
+			deck.save()
+	else:
+		form = DeckForm(instance=deck)
+	
+	context={'form': form, 'card_list':card_list,'deck':deck,'enough':enough,}
+	return render(request, 'deck_edit.html', context)
 
 def card_rank(request):
 	return render_to_response('card_rank.html', locals())
